ec5b001d90ffdb294fb34b0331fef2f987de01a7
Yejin Jang <jyjin0215@gmail.com>
2022-01-13 14:36:42 -0500

    LS03 vscode walkthrough

---
+++ lessons/00_stored_program.py
@@ -0,0 +1,8 @@
+"""An example, simple progrram. <- adding notes on top to describe what the program is.
+#writing codes are writing a recipe. A computer is not acting on the program immediately.
+#Let's follow the instrction with terminal! """
+
+print("Hello, World.")
+quiz: int = 100
+print(quiz)
+print("Goodbye.")
